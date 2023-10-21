407ff5f768c54f6bafa892d4cc0eec2770afc898
ghhzshivam <shivamjd365@gmail.com>
2023-10-21 14:02:56 +0530

    CI-CD commit

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predict_datapoint():
         return render_template('predict.html', results = e)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port = 8080)
